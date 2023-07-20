851356c80355d77b0d813829607e91dbb835f491
suuuuuu01 <138651545+suuuuuu01@users.noreply.github.com>
2023-07-20 17:38:21 +0900

    0720

---
+++ javascript/module/message.js
@@ -0,0 +1,7 @@
+const message = () => {
+    const name = "홍길동";
+    const age = 100;
+    return name + "은 나이가 " + age + "세 입니다.";
+};
+
+export default message; // 함수를 외부에서 참조가능하게 함
---
+++ javascript/module/myScript.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "웹자바개발자 과정";
+}
